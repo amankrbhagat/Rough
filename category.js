30c5a197c907ba9c8485d3b57f54e9088f1e577a
amankrbhagat <115861752+amankrbhagat@users.noreply.github.com>
2025-05-14 17:51:39 +0530

    Add files via upload

--- category.js
+++ category.js
@@ -1,40 +1,6 @@
-const express = require("express");
-const router = express.Router();
-const Category = require("./../db/category");
-const {
-  addCategory,
-  updateCategory,
-  deleteCategory,
-  getCategories,
-  getCategoryById,
-} = require("../handlers/category-handler");
-router.post("", async (req, res) => {
-  console.log("here");
-  let model = req.body;
-  let result = await addCategory(model);
-  res.send(result);
+const mongoose = require("mongoose");
+const categorySchema = new mongoose.Schema({
+  name: String,
 });
-
-router.get("", async (req, res) => {
-  let result = await getCategories();
-  res.send(result);
-});
-
-router.get("/:id", async (req, res) => {
-  let id = req.params["id"];
-  let result = await getCategoryById(id);
-  res.send(result);
-});
-router.put("/:id", async (req, res) => {
-  let model = req.body;
-  let id = req.params["id"];
-  await updateCategory(id, model);
-  res.send({ message: "updated" });
-});
-router.delete("/:id", async (req, res) => {
-  let id = req.params["id"];
-  await deleteCategory(id);
-  res.send({ message: "deleted" });
-});
-
-module.exports = router;
+const Category = mongoose.model("categories", categorySchema);
+module.exports = Category;
